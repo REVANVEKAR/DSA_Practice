d526d0cb644b61cef5b31fa8ff3fa3a49d390cbf
Revanth N Anvekar <revanthanvekar11@gmail.com>
2023-11-02 00:26:10 +0530

    patterns

--- revise/patterns.java
+++ revise/patterns.java
@@ -1,20 +1,6 @@
 package revise;
 
 public class patterns {
-    public static void main(String[] args) {
-//        System.out.println("hello");
-//        pattern_1(3);
-//        pattern_2(5);
-//        pattern_3(4);
-//        pattern_4(4);
-//        pattern_5(5);
-        printTriangleInverse(6);
-    }
-
-
-
-
-
 
     public static void pattern_1(int n ){
         int i , j;
@@ -85,16 +71,59 @@ public class patterns {
 
     static void printTriangleInverse(int n){
         for (int i = 1 ; i<=n ; i++ ){
-            for (int j = n-i ; j<=1 ; j--){
+            for (int j = 0 ; j < i ; j++){
                 System.out.print(" ");
             }
-            for (int j = 2*i-1 ; j>= 1 ; j--){
+            for (int j = 0 ; j<2*n-(2*i+1) ; j++){
                 System.out.print("*");
+            }
+            for (int j = 0 ; j < i ; j++){
+                System.out.print(" ");
             }
 
             System.out.println();
         }
     }
 
+    static void pattern_10(int n){
+        for (int i = 1; i<=2*n-1 ; i++){
+            int stars = i;
+            if (i>n){
+                stars = 2*n - i;
+            }
+            for (int j = 1; j<=stars ; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    static void pattern_11(int n){
+        int start = 1;
+        for (int i = 0; i <= n ; i++){
+            if (i%2 == 0){
+                start = 0;
+            }else{
+                start = 1;
+            }
+            for (int j = 0; j <i ; j++){
+                System.out.print(start);
+                start = 1 -start;
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+//        System.out.println("hello");
+//        pattern_1(3);
+//        pattern_2(5);
+//        pattern_3(4);
+//        pattern_4(4);
+//        pattern_5(5);
+//        printTriangleInverse(6);
+//        pattern_10(5);
+        pattern_11(5);
+    }
 
 }
