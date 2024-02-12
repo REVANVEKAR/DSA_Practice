57271d622ef55b02677f01a46e3f67b78c3904ac
Revanth N Anvekar <revanthanvekar11@gmail.com>
2024-02-13 02:30:12 +0530

    bs thoda hogya

--- revise/BinaryTrees/binaryTrees.java
+++ revise/BinaryTrees/binaryTrees.java
@@ -3,45 +3,44 @@ package revise.BinaryTrees;
 import com.sun.source.tree.Tree;
 
 import javax.sound.midi.MidiFileFormat;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Stack;
+import java.awt.image.AreaAveragingScaleFilter;
+import java.util.*;
 
 public class binaryTrees {
 
     public List<Integer> preorderTraversal(TreeNode root) {
         List<Integer> list = new ArrayList<>();
-        traverse(root,list);
+        traverse(root, list);
 
         return list;
     }
 
     private void traverse(TreeNode root, List<Integer> list) {
-        if (root==null){
+        if (root == null) {
             return;
         }
         list.add(root.val);
-        traverse(root.left,list);
-        traverse(root.right,list);
+        traverse(root.left, list);
+        traverse(root.right, list);
     }
 
-//    144. Binary Tree Preorder Traversal
+    //    144. Binary Tree Preorder Traversal
     public List<Integer> preorderTraversalIterative(TreeNode root) {
         List<Integer> list = new ArrayList<>();
-        if (root == null){
+        if (root == null) {
             return list;
         }
 
         Stack<TreeNode> stack = new Stack<>();
         stack.push(root);
-        while (!stack.isEmpty()){
+        while (!stack.isEmpty()) {
             TreeNode temp = stack.pop();
             list.add(temp.val);
 
-            if (temp.right != null){
+            if (temp.right != null) {
                 stack.push(temp.right);
             }
-            if (temp.left != null){
+            if (temp.left != null) {
                 stack.push(temp.left);
             }
         }
@@ -51,52 +50,102 @@ public class binaryTrees {
 
     public List<Integer> inorderTraversal(TreeNode root) {
         List<Integer> list = new ArrayList<>();
-        inorderTraversalHelper(root,list);
+        inorderTraversalHelper(root, list);
 
         return list;
     }
 
-    public void inorderTraversalHelper(TreeNode root , List<Integer> list){
-        if (root == null){
+    public void inorderTraversalHelper(TreeNode root, List<Integer> list) {
+        if (root == null) {
             return;
         }
 
         list.add(root.val);
         inorderTraversalHelper(root.right, list);
-        inorderTraversalHelper(root.left,list);
+        inorderTraversalHelper(root.left, list);
 
     }
 
 
-//    94. Binary Tree Inorder Traversal
+    //    94. Binary Tree Inorder Traversal
     public List<Integer> inorderTraversalIterative(TreeNode root) {
 
         List<Integer> list = new ArrayList<>();
 
-        if (root == null){
+        if (root == null) {
             return list;
         }
         Stack<TreeNode> stack = new Stack<>();
         TreeNode temp = root;
-        while (true){
-            if (temp!=null){
+        while (true) {
+            if (temp != null) {
                 stack.push(temp);
-                temp=temp.left;
-            }else{
-                if (stack.isEmpty()){
+                temp = temp.left;
+            } else {
+                if (stack.isEmpty()) {
                     break;
                 }
                 temp = stack.pop();
                 list.add(temp.val);
-                temp= temp.right;
+                temp = temp.right;
             }
         }
         return list;
     }
 
 
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        postorderTraversalHelper(root, list);
 
+        return list;
+    }
 
+    public void postorderTraversalHelper(TreeNode root, List<Integer> list) {
+        if (root == null) {
+            return;
+        }
+
+        postorderTraversalHelper(root.left, list);
+        postorderTraversalHelper(root.right, list);
+        list.add(root.val);
+
+    }
+
+//    public List<Integer> postorderTraversalIterative(TreeNode root){
+//        List<Integer> list = new ArrayList<>();
+//
+//    } // continue tom
+
+    public List<List<Integer>> levelOrder(TreeNode root) {
+
+        List<List<Integer>> result = new ArrayList<>();
+        Queue<TreeNode> queue = new LinkedList<>();
+
+        if (root == null){
+            return result;
+        }
+
+        queue.add(root);
+
+        while (!queue.isEmpty()){
+            int levelNum = queue.size();
+            List<Integer> temp = new ArrayList<>();
+            for (int i =0; i< levelNum ; i++){
+                if (queue.peek().left != null){
+                    queue.offer(queue.peek().left);
+                }
+                if (queue.peek().right != null){
+                    queue.offer(queue.peek().right);
+                }
+                temp.add(queue.poll().val);
+            }
+            result.add(temp);
+        }
+
+        return result;
+
+    }
 
 
     public static void main(String[] args) {
@@ -104,14 +153,22 @@ public class binaryTrees {
     }
 
     public class TreeNode {
-      int val;
-      TreeNode left;
-      TreeNode right;
-      TreeNode() {}
-      TreeNode(int val) { this.val = val; }
-      TreeNode(int val, TreeNode left, TreeNode right) {
-          this.val = val;
-          this.left = left;
-          this.right = right;
-      }
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
 }
