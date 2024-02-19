c80bdbbcd006698e9a0682be4aa054960b991ee1
Revanth N Anvekar <revanthanvekar11@gmail.com>
2024-02-20 01:25:10 +0530

    bt medium done

--- revise/BinaryTrees/binaryTrees.java
+++ revise/BinaryTrees/binaryTrees.java
@@ -361,6 +361,67 @@ public class binaryTrees {
         return result;
     }
 
+    public ArrayList <Integer> bottomView(Node root)
+    {
+        ArrayList<Integer> ans = new ArrayList<>();
+        if(root == null) return ans;
+        Map<Integer, Integer> map = new TreeMap<>();
+        Queue<Node> q = new LinkedList<Node>();
+        root.hd = 0;
+        q.add(root);
+        while(!q.isEmpty()) {
+            Node temp = q.remove();
+            int hd = temp.hd;
+            map.put(hd, temp.val);
+            if(temp.left != null) {
+                temp.left.hd = hd - 1;
+                q.add(temp.left);
+            }
+            if(temp.right != null) {
+                temp.right.hd = hd + 1;
+                q.add(temp.right);
+            }
+        }
+
+        for (Map.Entry<Integer,Integer> entry : map.entrySet()) {
+            ans.add(entry.getValue());
+        }
+        return ans;
+
+    }
+
+    static ArrayList<Integer> topView(Node root)
+    {
+        ArrayList<Integer> ans = new ArrayList<>();
+        if(root == null) return ans;
+        Map<Integer, Integer> map = new TreeMap<>();
+        Queue<Pair> q = new LinkedList<Pair>();
+        q.add(new Pair(root, 0));
+        while(!q.isEmpty()) {
+            Pair it = q.remove();
+            int hd = it.hd;
+            Node temp = it.node;
+            if(map.get(hd) == null) map.put(hd, temp.val);
+            if(temp.left != null) {
+
+                q.add(new Pair(temp.left, hd - 1));
+            }
+            if(temp.right != null) {
+
+                q.add(new Pair(temp.right, hd + 1));
+            }
+        }
+
+        for (Map.Entry<Integer,Integer> entry : map.entrySet()) {
+            ans.add(entry.getValue());
+        }
+        return ans;
+
+    }
+
+
+
+
 
 
 
@@ -368,6 +429,47 @@ public class binaryTrees {
     public static void main(String[] args) {
 
     }
+
+    public class Node{
+        int val;
+        Node left;
+        Node right;
+        int hd; // added for top and bottom problem or lets just create a new one
+
+        Node() {
+        }
+
+        Node(int val) {
+            this.val = val;
+        }
+
+        Node(int val, Node left, Node right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+
+        Node(int val, Node left, Node right, int hd) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+            this.hd = hd;
+        }
+
+
+    }
+
+    public static class Pair{
+        Node node;
+        int hd;
+
+        public Pair(Node root, int i) {
+            this.node  = root;
+            this.hd = i;
+        }
+    }
+
+
 
     public class TreeNode {
         int val;
